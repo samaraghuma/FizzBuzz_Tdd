bd0178317d98aee5a8479136b31f8b72b30032c3
raghuma sama <raghuma sama>
2016-12-30 00:19:07 -0500

    test failed for 15

--- nbattu.Library.Test/nbattuTests.cs
+++ nbattu.Library.Test/nbattuTests.cs
@@ -14,13 +14,13 @@ namespace FizzBuzz.Library.Tests
 
 
         [Test]
-        public void Buzzer_WhenDefault_ReturnsInput([Values(1, 2, 4, 7, 8)]int input)
+        public void Buzzer_WhenDefault_ReturnsInput([Values(1, 2, 4, 7, 8, 11, 13, 14)]int input)
         {
             string output = FizzBuzzer.Getvalue(input);
             Assert.AreEqual(input.ToString(), output);
         }
         [Test]
-        public void Buzzer_WhenDiv3_ReturnsFizz([Values(3, 6)] int input)
+        public void Buzzer_WhenDiv3_ReturnsFizz([Values(3, 6, 9, 12)] int input)
         {
 
             string output = FizzBuzzer.Getvalue(input);
@@ -32,6 +32,13 @@ namespace FizzBuzz.Library.Tests
 
             string output = FizzBuzzer.Getvalue(input);
             Assert.AreEqual("Buzz", output);
+        }
+        [Test]
+        public void Buzzer_WhenDiv3and5_ReturnsFizzBuzz([Values(15)] int input)
+        {
+
+            string output = FizzBuzzer.Getvalue(input);
+            Assert.AreEqual("FizzBuzz", output);
         }
     }
 }
