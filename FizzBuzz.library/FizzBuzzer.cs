488ba80ae28567e0e96d4b2cadbd9b0103ce9a3e
raghuma sama <raghuma sama>
2016-12-30 00:13:33 -0500

    test passed for 10

--- FizzBuzz.library/FizzBuzzer.cs
+++ FizzBuzz.library/FizzBuzzer.cs
@@ -12,7 +12,7 @@ namespace FizzBuzz.library
         {
             if (input % 3 == 0)
                 return "Fizz";
-            if (input == 5)
+            if (input % 5 == 0)
                 return "Buzz";
             return input.ToString();
         }
